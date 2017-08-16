a243806f45bc0e06306b7840c4968d502eab1abb
David Mendez Guardado <demg@outlook.com>
2017-08-16 16:47:39 -0500

    1.-Hola Mundo

--- TestPython/TestPython.py
+++ TestPython/TestPython.py
@@ -1,3 +1,7 @@
 #!usr/bin/python
 # El siguiente codigo es la prueba de como aprender Python facil y rapido en 3 dias,
 # Se utilizo el curso del Dr. Chuck https://online.dr-chuck.com/index.php
+
+# Dia 1
+
+print("Hello World");
